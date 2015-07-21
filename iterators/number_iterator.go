f7fe817ff695953d38ce848cdb63a7fa21bce22f
Lila Kurse <lilakurse@gmail.com>
2015-07-21 17:41:28 +0300

    added func check, name ,count

--- iterators/number_iterator.go
+++ iterators/number_iterator.go
@@ -1,7 +1,7 @@
 package iterators
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/GabbyyLS/data-linter/linter"
 	"math/rand"
 	"time"
@@ -11,17 +11,29 @@ import (
 // Each Number is a Checker, i.e., it has the Check() method implemented.
 type Number int
 type Generator struct {
-	Name  *linter.Report
-	Count *linter.Count
-	Next  *linter.Checker
+	Name  string
+	Count int64
 }
 
 func (Number) Check() ([]*linter.Problem, error) {
-	// TODO
+	problem :=[]linter.Problem{}
+	for _,value:=range checkers{
+		if value/2 !=0 {
+			return append(problem, "not even"), error
+		}
+	}
+	return nil,nil
 }
 
 func (g *Generator) Name() string {
-	return fmt.Sprintf("This is a test random number generator")
+	testGenerator := Generator{Name:"Test generator", Count:1000}
+	//return "This is a test random number generator"
+	return testGenerator.Name
+}
+
+func (g *Generator) Count() int64{
+	testGenerator := Generator{Name:"Test generator", Count:1000}
+	return testGenerator.Count
 }
 
 func (g *Generator) Next(Step int) []linter.Checker {
@@ -31,7 +43,10 @@ func (g *Generator) Next(Step int) []linter.Checker {
 		k := rand.Int()
 		checker := Number(k)
 		checkers = append(checkers, checker)
+
 	}
+//	if Step>Count()
+//		return nil
 	return checkers
 	// TODO: add some checking against Count
 }
