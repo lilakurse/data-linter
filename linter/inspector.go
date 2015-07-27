f55b27b6a42e3cd30862eec817ef57ec592e25b2
Lila Kurse <lilakurse@gmail.com>
2015-07-27 18:25:10 +0300

    added one og the solution to Inspect

--- linter/inspector.go
+++ linter/inspector.go
@@ -5,13 +5,13 @@ const Step = 50
 // TODO: check logic (step 4)
 // Inspect runs through the iterator and collects problems for report.
 func Inspect(iterator Iterator) ([]*Problem, error) {
-
+	//check := Checker()
 	docsChecked := 0
 	problemsToCommit := []*Problem{}
-
 	for docsChecked <= iterator.Count() {
 		for _, document := range iterator.Next(Step) {
-			docProblems, err := document.Check() // TODO: fix this
+			document = Checker().Check()  // We need to do document checker and then call Check() but i m not sure about that.// TODO: fix
+			docProblems, err := document
 			if err != nil {
 				return nil, err
 			}
