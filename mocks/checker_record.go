9c765558839729776d878b68d06a97cb6024be82
Lila Kurse <lilakurse@gmail.com>
2015-07-28 16:33:46 +0300

    added mock_vars.go and changed varibles name

--- mocks/checker_record.go
+++ mocks/checker_record.go
@@ -9,8 +9,8 @@ func NewMockChecker() *Checker {
 	mockObj := new(Checker)
 
 	// Tests for Checker()
-	mockObj.On("Check", mock.valid).Return(nil)
-	mockObj.On("Check", mock.invalid).Return(errors.New("invalid document"))
+	mockObj.On("Check", mock.validDoc).Return(nil)
+	mockObj.On("Check", mock.invalidDoc).Return(errors.New("invalid document"))
 
 	return mockObj
 }
