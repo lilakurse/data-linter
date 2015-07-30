0a0f31b9cb6ca2959ebbb0091310cda4430f0945
Alex Boklin <alex.boklin@gmail.com>
2015-07-30 11:51:12 +0300

    commented out mock vars -- we first need to define our expectations in record files

--- mock/mock_vars.go
+++ mock/mock_vars.go
@@ -2,14 +2,14 @@ package mock
 
 import "github.com/GabbyyLS/data-linter/linter"
 
-type Document struct {
-	Name string
-}
+//type Document struct {
+//	Name string
+//}
 
 var (
-	InvalidDoc = &Document{Name: "This is an invalid document"}
-	ValidDoc   = &Document{Name: "This is a valid document"}
-	BadDoc     = &Document{Name: "666"}
-	Problem    = linter.Problem{Original: InvalidDoc.Name}
-
+// TODO: let's switch to these as soon as we're done with writing record files
+//	InvalidDoc = &Document{Name: "This is an invalid document"}
+//	ValidDoc   = &Document{Name: "This is a valid document"}
+//	BadDoc     = &Document{Name: "666"}
+//	Problem    = linter.Problem{Original: InvalidDoc.Name}
 )
