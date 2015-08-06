5c68df299a9c9ee8ff88cf0cbe91ba18c97b4832
Lila Kurse <lilakurse@gmail.com>
2015-08-06 15:51:31 +0300

    added test for ReportWriter

--- linter/reportwriter_test.go
+++ linter/reportwriter_test.go
@@ -12,5 +12,32 @@ var (
 )
 
 func TestStart(t *testing.T) {
+	// ReportWriter with empty list of problems
+	start, err := Start(mockReportWriter)
+	if err != nil {
+		return t.Error("Report has problems, the list of problems shoud be empty")
+	}
+	if start.Statistics.Total != start.Statistics.Inspected || start.Statistics.Valid == 0 {
+		return t.Errorf("Report has problems")
+	}
+	// ReportWriter with some problems
+	start, err = Start(mockReportWriterWithProblem)
+	if err != nil {
+		return t.Errorf("Error")
+	}
+	if start.Statistics.Total != start.Statistics.Inspected || start.Statistics.Invalid == 0 { //not sure about second part
+		return t.Errorf("Report has problems")
+	}
+	// ReportWriter with errors
+	start, err = Start(mockReportWriterWithError)
+	if err != nil {
+		return t.Errorf("Error")
+	}
+	if start.Statistics.Total == start.Statistics.Inspected {
+		return t.Errorf("Statistics should not be equal")
+	}
+}
 
+func TestFinish(t *testing.T) {
+	//
 }
--- mock/problem.go
+++ mock/problem.go
@@ -7,11 +7,11 @@ import (
 
 var (
 	StatisticS             = models.Count{Total: 50, Inspected: 50, Valid: 25, Invalid: 25}
-	BadStatisticS          = models.Count{Total: 50, Inspected: 10, Valid: 5, Invalid: 5}
-	time                   = time.Now()
+	UnfinishedStatistics   = models.Count{Total: 50, Inspected: 10, Valid: 5, Invalid: 5}
+	ReportTime             = time.Now()
 	EmptyProblemList       = []*models.Problem{}
 	Problems               = []*models.Problem{{Original: "A file with a problem"}, {Original: "Another file with a problem"}}
-	ReportWithNoProblems   = *models.Report{Name: "Customers", Created: time, Updated: time + 2, Finished: time + 3, Error: "No error", Problems: EmptyProblemList, Statistics: StatisticS}
-	UnfinishedReport       = *models.Report{Name: "Orders", Created: time + 3, Updated: time + 4, Error: "Error occured while collecting problems", Problems: Problems, Statistics: BadStatisticS}
-	ReportWithSomeProblems = *models.Report{Name: "Freelancers", Created: time + 1, Updated: time + 2, Finished: time + 3, Error: "Some problems found", Statistics: StatisticS}
+	ReportWithNoProblems   = *models.Report{Name: "Customers", Created: ReportTime, Updated: ReportTime, Finished: ReportTime, Error: "No error", Problems: EmptyProblemList, Statistics: StatisticS}
+	UnfinishedReport       = *models.Report{Name: "Orders", Created: ReportTime, Updated: ReportTime, Error: "Error occured while collecting problems", Problems: Problems, Statistics: UnfinishedStatistics}
+	ReportWithSomeProblems = *models.Report{Name: "Freelancers", Created: ReportTime, Updated: ReportTime, Finished: ReportTime, Error: "Some problems found", Statistics: StatisticS}
 )
