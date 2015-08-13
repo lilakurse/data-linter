af33f1049197b9e5b1bbda4927c7e9279748d094
Lila Kurse <lilakurse@gmail.com>
2015-08-13 13:05:41 +0300

    write all  tests for ReportReader

--- linter/reporter_test.go
+++ linter/reporter_test.go
@@ -11,6 +11,9 @@ var (
 	mockReportWriterWithProblem = ReportWriter(mocks.NewMockRepotWriterWithProblems())
 	mockReportWriterWithError   = ReportWriter(mocks.NewMockReportWriterWithError())
 	mockAllReportReader         = ReportReader(mocks.NewMockAllReportReader())
+	mockReportReaderNoProblem   = ReportReader((mocks.NewMockReportReaderNoProblem()))
+	mockReportReaderWithProblem = ReportReader(mocks.NewMockReportReaderWithProblem())
+	mockReportReaderWithError   = ReportReader(mocks.NewMockReportReaderWithError())
 )
 
 func TestStart(t *testing.T) {
@@ -20,7 +23,7 @@ func TestStart(t *testing.T) {
 		t.Errorf("This report should have no problems")
 	}
 	if report.Statistics.Total != report.Statistics.Inspected ||
-									report.Statistics.Valid != report.Statistics.Inspected {
+		report.Statistics.Valid == 0 {
 		t.Errorf("Report has problems")
 	}
 	// ReportWriter with some problems
@@ -88,3 +91,63 @@ func TestGetAllReports(t *testing.T) {
 //		t.Errorf("Total should be equal to the sum of successful and failed")
 //	}
 //}
+
+func TestGetReportByName(t *testing.T) {
+	report := mockReportReaderNoProblem.GetReportByName(mock.ReportWithNoProblems.Name)
+	if report.Name == "" {
+		t.Errorf("Report should have a name")
+	}
+	report = mockReportReaderWithProblem.GetReportByName(mock.ReportWithSomeProblems.Name)
+	if report.Name == "" {
+		t.Errorf("Report should have a name")
+	}
+	report = mockReportReaderWithError.GetReportByName(mock.UnfinishedReport.Name)
+	if report.Name == "" {
+		t.Errorf("Report should have a name")
+	}
+}
+
+func TestGetReportByCreationTime(t *testing.T) {
+	report := mockReportReaderNoProblem.GetReportByCreationTime(mock.ReportWithNoProblems.Created)
+	if report.Created == nil {
+		t.Errorf("Report doesn't exist")
+	}
+	report = mockReportReaderWithProblem.GetReportByCreationTime(mock.ReportWithSomeProblems.Created)
+	if report.Created == nil {
+		t.Errorf("Report doesn't exist")
+	}
+	report = mockReportReaderWithError.GetReportByCreationTime(mock.UnfinishedReport.Created)
+	if report.Created == nil {
+		t.Errorf("Report doesn't exist")
+	}
+}
+
+func TestGetReportByUpdateTime(t *testing.T) {
+	report := mockReportReaderNoProblem.GetReportByUpdateTime(mock.ReportWithNoProblems.Updated)
+	if report.Updated == nil {
+		t.Errorf("Report is old")
+	}
+	report = mockReportReaderWithProblem.GetReportByUpdateTime(mock.ReportWithSomeProblems.Updated)
+	if report.Updated == nil {
+		t.Errorf("Report is old")
+	}
+	report = mockReportReaderWithError.GetReportByUpdateTime(mock.UnfinishedReport.Updated)
+	if report.Updated == nil {
+		t.Errorf("Report is old")
+	}
+}
+
+func TestGetReportByCommitTime(t *testing.T) {
+	report := mockReportReaderNoProblem.GetReportByCommitTime(mock.ReportWithNoProblems.Finished)
+	if report.Finished == nil {
+		t.Errorf("Report has some problems")
+	}
+	report = mockReportReaderWithProblem.GetReportByCommitTime(mock.ReportWithSomeProblems.Finished)
+	if report.Finished == nil {
+		t.Errorf("Report has some problems")
+	}
+	report = mockReportReaderWithError.GetReportByCommitTime(mock.UnfinishedReport.Finished)
+	if report.Finished != nil {
+		t.Errorf("Finished time should be nil")
+	}
+}
