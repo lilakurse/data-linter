d8475a717a77b1562f09850ec4c09af3e55c0e0a
Alex Boklin <alex.boklin@gmail.com>
2015-08-20 15:32:00 +0300

    cleaned code in iterator.go

--- linter/iterator.go
+++ linter/iterator.go
@@ -11,25 +11,13 @@ type Iterator interface {
 	Next(step int) []*checker.Checker
 }
 
-//func Name(ctx context.Context) string {
-//	return IteratorFromContext(ctx).Name()
-//}
-
 func Name(iterator Iterator) string {
 	return iterator.Name()
 }
 
-//func Count(ctx context.Context) int64{
-//	return IteratorFromContext(ctx).Count()
-//}
-
 func Count(iterator Iterator) int{
 	return iterator.Count()
 }
-
-//func Next(ctx context.Context, step int) []*Checker {
-//	return IteratorFromContext(ctx).Next(step)
-//}
 
 func Next(iterator Iterator, step int) []*checker.Checker {
 	return iterator.Next(step)
